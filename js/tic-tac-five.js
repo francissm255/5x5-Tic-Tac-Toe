e49b5e156047547ac7d948c0267ebbc5a7476a13
Sean Miera <francissm255@gmail.com>
2024-03-13 18:37:18 -0600

    5x5 board with placeable pieces and css stylesheet.

---
+++ js/tic-tac-five.js
@@ -0,0 +1,54 @@
+document.getElementById('start').addEventListener('click',startGame);
+
+let turn;
+
+function startGame(){
+  setTurn();
+  setTableListeners();
+  document.getElementById('start').removeEventListener('click',startGame);
+}
+
+function setTableListeners(){
+  const cells = document.getElementsByTagName('td');
+  for(let c of cells){
+    c.addEventListener('click',setContent);
+    c.addEventListener('mouseover',enter);
+    c.addEventListener('mouseout',exit);
+  }
+}
+
+function enter(){
+  setColor(this,'lightgreen');
+}
+
+function exit(){
+  setColor(this,'lightgrey');
+}
+
+function setContent(){
+  this.textContent = turn;
+  setTurn();
+  this.removeEventListener('click', setContent);
+  this.removeEventListener('mouseover', enter);
+  this.removeEventListener('mouseout', exit);
+  setColor(this,'lightgrey');
+}
+
+function setTurn(){
+  const x = document.getElementById('x');
+  const o = document.getElementById('o');
+  if(turn === 'x'){
+    turn = 'o';
+    setColor(o, 'lightgreen');
+    setColor(x, 'lightgrey');
+  }else{
+    turn = 'x';
+    setColor(x, 'lightgreen');
+    setColor(o, 'lightgrey');
+  }
+}
+
+function setColor(node, color){
+  node.style.backgroundColor = color;
+}
+
